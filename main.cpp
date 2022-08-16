b74b9e7b0a9c1f027cfa0a2c6b6344be8c1ef75e
cmcdev-code <74151925+cmcdev-code@users.noreply.github.com>
2022-08-16 14:35:20 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "declerations.h"
+#include <iostream>
+
+using namespace std;
+using namespace decleration;
+
+
+int main(){
+    
+    debtor Collin;
+    Collin.loanOptFunc();
+
+
+
+    return 0;
+}
