5e452b2485b780d2361c0099eb790a4e33b8797a
cmcdev-code <74151925+cmcdev-code@users.noreply.github.com>
2022-08-16 14:36:04 -0400

    Create declarations.h

---
+++ declarations.h
@@ -0,0 +1,68 @@
+ #pragma once
+#include <iostream>
+
+
+/*Found how to define namespace here 
+https://docs.microsoft.com/en-us/cpp/cpp/header-files-cpp?view=msvc-170
+*/
+namespace decleration{
+
+//Setting the different loan options
+const int tenYear(10), fifteenYear(15),thirtyYear(30);
+
+
+
+class debtor
+{
+    //private:
+    
+        //The Option that the person chose
+        int loanOption;
+
+        //The loan ammount
+        double loanAmmount;
+
+        //The Yearly intrest
+        double yearlyIntrest;
+
+
+        
+   public:
+
+        //Constructor to initilize the memory when creating an object of the class 
+        debtor()
+            {
+                loanOption = 10;//Loan Option can't == 0 because then the amortization formula would be dividing by 0  
+                loanAmmount = 0.0f;
+                yearlyIntrest = 0.0f;
+            }
+        void setLoan(int s);
+
+
+        void setLoanOption(int& loanOption);
+        int getLoanOption();
+
+        void setLoanAmmount(double& loanAmmount);
+        double getLoanAmmount();
+
+
+        void setYearlyIntrest(double& yearlyIntrest);
+        double getYearlyIntrest();
+
+        
+
+        //Function that asks for which year loan they want and returns that option
+        int loanOptFunc();
+
+        //Function that will print out the different loan options
+        void toTerminal(int one,int two, int three);
+
+        //Function that will calc the number of intrest Thing
+        double numberInFunction(int year, float intrest);
+
+        //Function that will calc the Monthly payment
+        float amortizationCalc(double principle,float intrest,int year);
+
+
+};
+}
