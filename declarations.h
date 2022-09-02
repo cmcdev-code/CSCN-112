271125ce7a09e4c35079b97eaa8c8a2ed07e8e47
cmcdev-code <74151925+cmcdev-code@users.noreply.github.com>
2022-09-01 23:23:24 -0400

    Update declarations.h

--- declarations.h
+++ declarations.h
@@ -1,19 +1,13 @@
 #include <iostream>
 
-//Setting the different loan options
-const int tenYear(10), fifteenYear(15),thirtyYear(30);
-/*Found how to define namespace here 
-https://docs.microsoft.com/en-us/cpp/cpp/header-files-cpp?view=msvc-170
-*/
-namespace decleration{
-
 class debtor
 {
-    public:
-    //private:
+    
+    private:
     
         //The Option that the person chose
         int loanOption;
+        int paymentPeriods;
 
         //The loan ammount
         double loanAmmount;
@@ -21,44 +15,49 @@ class debtor
         //The Yearly intrest
         double yearlyIntrest;
 
+        //Function that will calc the Monthly payment
+        double amortizationCalc(double principle,double intrest,int year);//not ment to be called instead call the double callOfAmortizationCalc();
 
-        
+        double paymentCalculator(double monthlyPayment,double principal,double monthlyIntrest,int loanOption);//not ment to be called call the double callOf__paymentCalculator(); instead
+
    public:
 
         //Constructor to initilize the memory when creating an object of the class 
         debtor()
             {
-                loanOption = 10;//Loan Option can't == 0 because then the amortization formula would be dividing by 0  
-                loanAmmount = 0.0f;
+                loanOption = 10;//Loan Option can't == 0 because then the amortization formula would be dividing by 0 number^0 power would be 1 and the denomanator subtracts 1  
+                loanAmmount = 0.0f;//f in order to show that it is a floating point 
                 yearlyIntrest = 0.0f;
+
             }
-        void print(std::string s);
+        //Methods that will ask for the user input for the three inputs: principal ammount, loan option, and yearly interest.
+        double inOfPrincipalAmmount();
+        double inYearlyIntrest();
+        int inLoanOption();
 
+        //method that will call all the user inputs and then use the set methods bellow to set tha values to the corisponding ones
+        void in__of__all__user__inputs();
 
-        void setLoanOption(int& loanOption);
+        //Set and get methods
+        //******************************************************************
+        void setLoanOption(int loanOption);
         int getLoanOption();
 
-        void setLoanAmmount(double& loanAmmount);
+        void setLoanAmmount(double loanAmmount);
         double getLoanAmmount();
-
-
-        void setYearlyIntrest(double& yearlyIntrest);
+        
+        void setYearlyIntrest(double yearlyIntrest);
         double getYearlyIntrest();
 
+        void setPaymemtPeriods();
+        int getPaymentPeriods();
+
+        //******************************************************************
+
+        //Method that will call on the method bellow and also use the get methods 
+        double callOfAmortizationCalc();
+
+        //method that will calculate the montly payments 
+        double callOf__paymentCalculator();
         
-
-        //Function that asks for which year loan they want and returns that option
-        int loanOptFunc();
-
-        //Function that will print out the different loan options
-        void toTerminal(int one,int two, int three);
-
-        //Function that will calc the number of intrest Thing
-        double numberInFunction(int year, float intrest);
-
-        //Function that will calc the Monthly payment
-        float amortizationCalc(double principle,float intrest,int year);
-
-
 };
-}
