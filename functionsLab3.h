b983ec637b0c79375d78baa9fcc27175e39be582
cmcdev-code <74151925+cmcdev-code@users.noreply.github.com>
2022-09-25 16:04:59 -0400

    Update functionsLab3.h

--- functionsLab3.h
+++ functionsLab3.h
@@ -1,1 +1,43 @@
 
+#include <vector>
+#include <string>
+
+class  Employee
+{
+    public:
+    //set and get member functions 
+        void setName(std::string name);
+        std::string getName() const;
+
+        void setCity(std::string city);
+        std::string getCity() const;
+
+        void setId(std::string id);
+        std::string getId() const;
+
+    private://private member variables 
+        std::string name;
+        std::string city;
+        std::string id;
+        std::string fileName;
+
+        std::vector<double> scores;
+
+    protected:
+        void setfileName(std::string fileName);
+        std::string getfileName() const;
+
+};
+class userInput: public Employee{//inheritaed class that will handel the user inputs and filer manipultaion
+
+    public:
+        std::string fileInputName();
+        double scoreIn();
+
+
+
+
+
+
+
+};
