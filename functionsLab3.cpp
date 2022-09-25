516c6d1fa7e1e112af583b5a0c33e76d3217189d
cmcdev-code <74151925+cmcdev-code@users.noreply.github.com>
2022-09-25 16:14:45 -0400

    Create functionsLab3.cpp

---
+++ functionsLab3.cpp
@@ -0,0 +1,32 @@
+#include "function.h"
+#include <string>
+#include <fstream>
+
+//set member functions bellow 
+
+void Employee::setName(std::string name){
+    this->name = name;
+}
+void Employee::setCity(std::string city){
+    this->city =city;
+}
+void Employee::setId(std::string id){
+    this->id =id;
+}
+void Employee::setfileName(std::string fileName){
+    this->fileName=fileName;
+}
+
+
+//get member functions 
+
+
+
+
+
+
+
+
+
+
+
