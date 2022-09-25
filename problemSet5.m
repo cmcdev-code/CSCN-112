fe281467c2ffbd37265cf8885773d88f35529a1e
cmcdev-code <74151925+cmcdev-code@users.noreply.github.com>
2022-09-24 22:16:58 -0400

    Update problemSet5.m

--- problemSet5.m
+++ problemSet5.m
@@ -1,4 +1,4 @@
- clc,clear,close all
+clc,clear,close all
 Problem Set: Branching & Looping Assignment
 Collin McDevitt
 9/24/2022
@@ -32,8 +32,25 @@ fprintf('%1.0f',a)
 
 Problem 4.31 
 clc,clear,close all
+31. a. An ideal diode blocks the flow of current in the direction opposite that of the diode’s arrow symbol. It can be used to make a half-wave rectifier, as shown in Figure P31a. For the ideal diode, the voltage vL across the load RL is given by
 
 
+Suppose the supply voltage is
+
+where time t is in seconds. Write a MATLAB program to plot the voltage vL versus t for .
+creating the array 
+t=linspace(0,10,10000);
+V=3*exp(-t/3).*sin(t.*pi);
+Vl1=linspace(0,10,10000);
+Vl2=linspace(0,10,10000);
+Now to get the array with the piecewisefunction 
+for i=10000
+    if V(i)>0
+        Vl1(i)=V(i)
+    elseif V(i)<=0
+        Vl1(i)=0
+    end
+    
 
 
 
