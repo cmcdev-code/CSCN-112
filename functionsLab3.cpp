8502825529c951a130cc550c928502378e9a69f0
cmcdev-code <74151925+cmcdev-code@users.noreply.github.com>
2022-09-25 16:43:07 -0400

    Update functionsLab3.cpp

--- functionsLab3.cpp
+++ functionsLab3.cpp
@@ -1,6 +1,7 @@
 #include "function.h"
 #include <string>
 #include <fstream>
+#include <iostream>
 
 //set member functions bellow 
 
@@ -18,15 +19,46 @@ void Employee::setfileName(std::string fileName){
 }
 
 
-//get member functions 
+//get member functions
+std::string Employee::getName() const{
+    return this->name;
+}
+std::string Employee::getCity() const{
+    return this->city;
+}
+std::string Employee::getId() const{
+    return this-> id;
+}
+std::string Employee::getfileName() const{
+    return this-> fileName;
+}
 
+//user input functions 
+std::string userInput::fileInputName(){
+    std::string fileNameLocal="";   
+    std::cout<<"What is the name of the file? Please enter the extension example if it is a txt enter name.txt. \n";
+    std::getline(std::cin,fileNameLocal);
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
 
+return fileNameLocal;
+}
+double userInput::scoreIn(){
+    double localScore=0;
+    while(true){
+        std::cout<<"What is the score of the user? \n";
+        std::cin>>localScore;
+        if( localScore>=1 && localScore<=5 && !(std::cin.fail())){
+            break;
+        }else{
+            std::cout<<"Error please enter a number greatr then or equal to 1 and also less then or equal to 5.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+        }
 
+    }
 
-
-
-
-
-
+    return localScore;
+}
 
 
