a31f275db6bca602a78d069733816bbc8b22d82f
cmcdev-code <74151925+cmcdev-code@users.noreply.github.com>
2022-09-25 17:33:21 -0400

    Update functionsLab3.cpp

--- functionsLab3.cpp
+++ functionsLab3.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 
 //set member functions bellow 
 
@@ -60,10 +61,24 @@ double userInput::scoreIn(){
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
         }
-
     }
-
     return localScore;
 }
+void userInput::printEmployeeInfo() const{
+    std::cout<<std::setw(60)<<std::setfill('*')<<'\n';
+    
+    std::cout<<std::setw(60)<<std::left<<"Employee name:"<<std::setfill('-')<<std::right<<getName()<<'\n';
+    std::cout<<std::setw(60)<<std::left<<"Employee name:"<<std::setfill('-')<<std::right<<getName()<<'\n';
 
 
+}
+// void room::showData(double gallonsNeeded,double surface_area) const{
+//     std::cout<<std::setw(60)<<std::left<<"The number of gallons of paint needed for this room is:"<<std::setfill('-')<<std::fixed<<std::setprecision(3)<<std::right<<gallonsNeeded<<"\n";
+//     std::cout<<std::setw(60)<<std::left<<"The number of gallons of paint needed in total is:"<<std::setfill('-')<<std::fixed<<std::setprecision(3)<<std::right<<getPaintNeeded()<<"\n";
+//     std::cout<<std::setw(60)<<std::left<<"The height of the room is:"<<std::setfill('-')<<std::fixed<<std::setprecision(2)<<std::right<<getHeight()<<'m'<<"\n";
+//     std::cout<<std::setw(60)<<std::left<<"The width of the room is:"<<std::setfill('-')<<std::fixed<<std::setprecision(2)<<std::right<<getWidth()<<'m'<<"\n";
+//     std::cout<<std::setw(60)<<std::left<<"The length of the room is:"<<std::setfill('-')<<std::fixed<<std::setprecision(2)<<std::right<<getLength()<<'m'<<"\n";
+//     std::cout<<std::setw(60)<<std::left<<"The volume of the room is:"<<std::setfill('-')<<std::fixed<<std::setprecision(2)<<std::right<<getVolume()<<"m^3"<<"\n";
+//     std::cout<<std::setw(60)<<std::left<<"The paintable surface area of the room is:"<<std::setfill('-')<<std::fixed<<std::setprecision(2)<<std::right<<surface_area<<"m^2"<<"\n";
+//     std::cout<<std::setw(60)<<std::left<<"The number of rooms painted is:"<<std::setfill('-')<<std::right<<getNumberOfRoomsPainted()<<"\n";
+// }
