0a1b68c266b1b70a53613fbb2caa945b1aaba73c
cmcdev-code <74151925+cmcdev-code@users.noreply.github.com>
2022-09-25 22:33:27 -0400

    Update functionsLab3.cpp

--- functionsLab3.cpp
+++ functionsLab3.cpp
@@ -1,30 +1,59 @@
-#include <vector>
+#include "employee.h"
+#include <iomanip> 
 #include <string>
 
-class Employee{
+void Employee::setName(std::string name){
+    this-> name=name;
+}
+void Employee::setCity(std::string city){
+    this-> city=city;
+}
+void Employee::setID(std::string ID){
+    this->ID=ID;
+}
+int Employee::sizeVectorScore() const{
+    return this->score.size();
+}
+void Employee::vectorPushBack(double score){
+    this->score.push_back(score);
+}
+double Employee::vectorAtIndex(int index) const{
+    return this->score.at(index);
+}
 
-    public:
-    void setName(std::string  name);
-    std::string getName() const;
-
-    void setCity(std::string city);
-    std::string getCity() const;
-
-    void setID(std::string ID);
-    std::string getID() const;
-
-    Employee();    
-    Employee(std::string name,std::string city,std::string ID);
-
-    void vectorPushBack(double score);
-    double vectorAtIndex(int index) const;
-    int sizeVectorScore() const;
 
 
-private:
-    std::string name;
-    std::string city;
-    std::string ID;
-    std::vector<double> score;
+Employee::Employee(){
+    name="";
+    city="";
+    ID="";
+}
 
-};
+
+
+
+Employee::Employee(std::string name,std::string city,std::string ID){
+    this->name=name;
+    this->city=city;
+    this->ID=ID;
+}
+std::string Employee::getName() const{
+    return this-> name;
+}
+std::string Employee::getCity() const{
+    return this-> city;
+}
+std::string Employee::getID() const{
+    return this->ID;
+}
+
+
+
+
+
+
+
+
+
+
+
