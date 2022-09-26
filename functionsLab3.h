cbbfaa55c6d2e7c2f8c9f24f73345c74ffdff823
cmcdev-code <74151925+cmcdev-code@users.noreply.github.com>
2022-09-25 22:33:42 -0400

    Update functionsLab3.h

--- functionsLab3.h
+++ functionsLab3.h
@@ -1,47 +1,30 @@
 #include <vector>
 #include <string>
 
-class  Employee
-{
-    public:
-    //set and get member functions 
-        void setName(std::string name);
-        std::string getName() const;
-
-        void setCity(std::string city);
-        std::string getCity() const;
-
-        void setId(std::string id);
-        std::string getId() const;
-
-        void setScore(double scores);
-        void setScore(double scores, int i);
-        
-        double getScore(int index) const;
-
-    private://private member variables 
-        std::string name;
-        std::string city;
-        std::string id;
-        std::string fileName;
-
-        std::vector<double> scores;
-
-    protected:
-        void setfileName(std::string fileName);
-        std::string getfileName() const;
-
-};
-class userInput: public Employee{//inheritaed class that will handel the user inputs and filer manipultaion
+class Employee{
 
     public:
-        std::string fileInputName();
-        double scoreIn();
+    void setName(std::string  name);
+    std::string getName() const;
+
+    void setCity(std::string city);
+    std::string getCity() const;
+
+    void setID(std::string ID);
+    std::string getID() const;
+
+    Employee();    
+    Employee(std::string name,std::string city,std::string ID);
+
+    void vectorPushBack(double score);
+    double vectorAtIndex(int index) const;
+    int sizeVectorScore() const;
 
 
-
-
-
-
+private:
+    std::string name;
+    std::string city;
+    std::string ID;
+    std::vector<double> score;
 
 };
